c1ad737793b926230a9611dd72d22a0633ff70f2
WeilanTao <64599029+WeilanTao@users.noreply.github.com>
2021-06-21 14:27:51 -0400

    basic logic v1

--- src/main.java
+++ src/main.java
@@ -9,8 +9,6 @@ public class main {
         boolean p2win=false;
         map m = new map();
 
-
-
             /**
              * // How to read from standard input.
              * System.err is likewise a PrintStream,
@@ -24,32 +22,49 @@ public class main {
                     new InputStreamReader(System.in));
             String s;
 
+//            m.printTable();
+        //TODO Fix this piece of logic
             while (true) {
-                if(draw){
+                if (draw) {
                     System.out.println("TIE!");
                     m.printTable();
                     break;
-                }else if(p1win){
-                    System.out.println("Congrats! P1 win!" );
+                } else if (p1win) {
+                    System.out.println("Congrats! P1 win!");
                     m.printTable();
                     break;
-                }else if(p2win){
+                } else if (p2win) {
                     System.out.println("congrats! P2 win!");
                     m.printTable();
                     break;
+                }else {
+                    int p=player1? 1:2;
+                    char f=player1? 'x':'o';
+                    System.out.println("Player"+p+"...Please place "+f+" or enter g for giving up \n You can use ';' to separate your input");
+                    while ((s = stdin.readLine()) != null && s.length() != 0) {
+                        try{
+                            String param[]= s.split(";");
+//                            System.out.println(param[0].getClass().getSimpleName());
+                            if(param.length!=3 || param[2].length()!=1){
+                                throw new Exception();
+                            }
+                            m.upDateTable(Integer.parseInt(param[0]), Integer.parseInt(param[1]), param[2].charAt(0));
+                            m.printTable();
+                            player1= !player1;
+                        }catch(Exception e){
+                            System.out.println("please make sure the row_index is in {2,3,4}; " +
+                                    "the col_index is in {1,3,5}; you can only place an '"+
+                                    f+"' in your turn \n If you want to give up, please enter g; " +
+                                    "if you want to restart, please enter r; if you want to exit, " +
+                                    "please enter e");
+                        }
+
+
+//                        m.printTable();
+//                        player1 = false;
+                    }
+
                 }
-                else if(player1 && (s = stdin.readLine()) != null && s.length() != 0){
-
-                    System.out.println("Player1...Please place x or enter g for giving up");
-//                    System.out.println(s);
-                    m.printTable();
-
-                }else if(!player1 && (s = stdin.readLine()) != null && s.length() != 0){
-                    System.out.println("Player1...Please place x or enter g for giving up");
-//                    System.out.println(s);
-                    m.printTable();
-                }
-
             }
 
         }
--- src/map.java
+++ src/map.java
@@ -1,9 +1,7 @@
-import com.sun.javafx.binding.StringFormatter;
-
 import static java.lang.String.valueOf;
 
 public class map {
-    private static final char i=(char)9;
+    private static final char i=(char)32;
     private char line1[];
     private char line2[];
     private char line3[];
@@ -17,9 +15,9 @@ public class map {
     }
 
     map(){
-        line1=new char[]{ '|',i,'|',i,'|',i,'|'};
-        line2=new char[]{ '|',i,'|',i,'|',i,'|'};
-        line3=new char[]{ '|',i,'|',i,'|',i,'|'};
+        line1=new char[]{ '|',i,i,i,'|',i,i,i,'|',i,i,i,'|'};
+        line2=new char[]{ '|',i,i,i,'|',i,i,i,'|',i,i,i,'|'};
+        line3=new char[]{ '|',i,i,i,'|',i,i,i,'|',i,i,i,'|'};
         table=new String[]{title, row, valueOf(line1), row, valueOf(line2),row,valueOf(line3),row};
         printTable();
 
@@ -31,13 +29,30 @@ public class map {
         }
     }
 
-    public void upDateTable(int lineN, int index, char flag){
-        if(lineN==0 || lineN%2!=0 || index%2==0 || flag!='x' ||flag!='o'){
-//            throw Exception
+    public void upDateTable(int r, int c, char f) throws Exception{
+//TODO Simpify the piece of logic ... string formater?
+        if(r!=0 && r%2==0 && c%2==0 &&( f=='x'||f=='o')) {
+            switch (r) {
+                case 2: {
+                    line1[c] = f;
+                    table[r]=valueOf(line1);
+                    break;
+                }
+                case 4: {
+                    line2[c] = f;
+                    table[r]=valueOf(line2);
+                    break;
+                }
+                case 6: {
+                    line3[c] = f;
+                    table[r]=valueOf(line3);
+                    break;
+                }
+                default:
+                    break;
+            }
         }else{
-
-
+            throw new Exception();
         }
-
     }
 }
