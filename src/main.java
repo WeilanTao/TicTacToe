065d56ee0649ee41fd0542ad2b32ac0a3ab4a836
WeilanTao <64599029+WeilanTao@users.noreply.github.com>
2021-06-22 11:41:41 -0400

    fix input logic

--- src/main.java
+++ src/main.java
@@ -4,9 +4,7 @@ public class main {
 
     public static void main(String[] args) throws IOException {
         boolean player1=true;
-        boolean draw=false;
-        boolean p1win=false;
-        boolean p2win=false;
+
         map m = new map();
 
             /**
@@ -22,49 +20,54 @@ public class main {
                     new InputStreamReader(System.in));
             String s;
 
-//            m.printTable();
-        //TODO Fix this piece of logic
             while (true) {
-                if (draw) {
-                    System.out.println("TIE!");
-                    m.printTable();
-                    break;
-                } else if (p1win) {
-                    System.out.println("Congrats! P1 win!");
-                    m.printTable();
-                    break;
-                } else if (p2win) {
-                    System.out.println("congrats! P2 win!");
-                    m.printTable();
-                    break;
-                }else {
-                    int p=player1? 1:2;
-                    char f=player1? 'x':'o';
-                    System.out.println("Player"+p+"...Please place "+f+" or enter g for giving up \n You can use ';' to separate your input");
-                    while ((s = stdin.readLine()) != null && s.length() != 0) {
-                        try{
-                            String param[]= s.split(";");
-//                            System.out.println(param[0].getClass().getSimpleName());
-                            if(param.length!=3 || param[2].length()!=1){
-                                throw new Exception();
-                            }
-                            m.upDateTable(Integer.parseInt(param[0]), Integer.parseInt(param[1]), param[2].charAt(0));
-                            m.printTable();
-                            player1= !player1;
-                        }catch(Exception e){
-                            System.out.println("please make sure the row_index is in {2,3,4}; " +
-                                    "the col_index is in {1,3,5}; you can only place an '"+
-                                    f+"' in your turn \n If you want to give up, please enter g; " +
-                                    "if you want to restart, please enter r; if you want to exit, " +
-                                    "please enter e");
-                        }
 
+                int p=player1? 1:2;
+                char f=player1? 'x':'o';
+                System.out.println("Player"+p+"...Please place row;colon;"+f+" or enter g for giving up");
+                s = stdin.readLine();
+                if()
 
-//                        m.printTable();
-//                        player1 = false;
+                while (s == null || s.length() == 0){
+                    System.out.println("Input can't be null");
+                    s = stdin.readLine();
+                }
+
+                try{
+                    String param[]= s.split(";");
+//                  System.out.println(param[0].getClass().getSimpleName());
+                    if(param.length!=3 || param[2].length()!=1){
+                        throw new Exception();
+                    }
+                    m.upDateTable(Integer.parseInt(param[0]), Integer.parseInt(param[1]), param[2].charAt(0));
+                    m.printTable();
+                    player1= !player1;
+
+                    if (m.getDraw()) {
+                        System.out.println("TIE!");
+                        m.printTable();
+                        break;
+                    } else if (m.getP1win()) {
+                        System.out.println("Congrats! P1 win!");
+                        m.printTable();
+                        break;
+                    } else if (m.getP2win()) {
+                        System.out.println("congrats! P2 win!");
+                        m.printTable();
+                        break;
                     }
 
+
+                }catch(Exception e){
+                    System.out.println("please make sure the row_index is in {1,2,3}; " +
+                            "the col_index is in {1,2,3}; you can only place an '"+
+                            f+"' in your turn \n If you want to give up, please enter g; " +
+                            "if you want to restart, please enter r; if you want to exit, " +
+                            "please enter e");
                 }
+
+
+
             }
 
         }
--- src/map.java
+++ src/map.java
@@ -6,21 +6,50 @@ public class map {
     private char line2[];
     private char line3[];
     public String table[];
-    private String title="*TIC TAC TOE*";
-    private String row=" --- --- ---";
-    private static final int TABLE_SIZE=8;
+    private static final String TITLE="*TIC TAC TOE*";
+    private static final String ROW=" --- --- ---";
+    private static final int TABLE_SIZE=9;
+    private static final String LINE_0="  1   2   3  ";
+    private boolean draw;
+    private boolean p1win;
+    private boolean p2win;
 
-    private class ParameterException extends Exception{
+
+    map(){
+        line1=new char[]{'|',i,i,i,'|',i,i,i,'|',i,i,i,'|','1'};
+        line2=new char[]{'|',i,i,i,'|',i,i,i,'|',i,i,i,'|','2'};
+        line3=new char[]{'|',i,i,i,'|',i,i,i,'|',i,i,i,'|','3'};
+        table=new String[]{TITLE, LINE_0, ROW, valueOf(line1), ROW, valueOf(line2),ROW,valueOf(line3),ROW};
+        draw=false;
+        p1win=false;
+        p2win=false;
+        printTable();
 
     }
 
-    map(){
-        line1=new char[]{ '|',i,i,i,'|',i,i,i,'|',i,i,i,'|'};
-        line2=new char[]{ '|',i,i,i,'|',i,i,i,'|',i,i,i,'|'};
-        line3=new char[]{ '|',i,i,i,'|',i,i,i,'|',i,i,i,'|'};
-        table=new String[]{title, row, valueOf(line1), row, valueOf(line2),row,valueOf(line3),row};
-        printTable();
+    public boolean getDraw() {
+        return draw;
+    }
 
+    public boolean getP1win() {
+        return p1win;
+    }
+
+    public boolean getP2win() {
+        return p2win;
+    }
+
+    public void setDraw(boolean draw) {
+        this.draw = draw;
+    }
+
+
+    public void setP1win(boolean p1win) {
+        this.p1win = p1win;
+    }
+
+    public void setP2win(boolean p2win) {
+        this.p2win = p2win;
     }
 
     public void printTable(){
@@ -30,27 +59,29 @@ public class map {
     }
 
     public void upDateTable(int r, int c, char f) throws Exception{
-//TODO Simpify the piece of logic ... string formater?
-        if(r!=0 && r%2==0 && c%2==0 &&( f=='x'||f=='o')) {
+//TODO Simpify the piece of logic ... string formater? and fix the inputs out of range;setDraw; setP1WIN; setP2WIN
+        if(  ( f=='x'||f=='o')) {
             switch (r) {
+                case 1: {
+                    line1[2+4*(c-1)] = f;
+                    table[3]=valueOf(line1);
+                    break;
+                }
                 case 2: {
-                    line1[c] = f;
-                    table[r]=valueOf(line1);
+                    line2[2+4*(c-1)] = f;
+                    table[5]=valueOf(line2);
                     break;
                 }
-                case 4: {
-                    line2[c] = f;
-                    table[r]=valueOf(line2);
-                    break;
-                }
-                case 6: {
-                    line3[c] = f;
-                    table[r]=valueOf(line3);
+                case 3: {
+                    line3[2+4*(c-1)] = f;
+                    table[7]=valueOf(line3);
                     break;
                 }
                 default:
                     break;
             }
+
+
         }else{
             throw new Exception();
         }
