477ea68c8f0ebef738dac97acf15c23fe755f493
WeilanTao <64599029+WeilanTao@users.noreply.github.com>
2021-06-22 13:05:21 -0400

    implement win/draw

--- src/main.java
+++ src/main.java
@@ -26,7 +26,21 @@ public class main {
                 char f=player1? 'x':'o';
                 System.out.println("Player"+p+"...Please place row;colon;"+f+" or enter g for giving up");
                 s = stdin.readLine();
-                if()
+                if(s.equals("i")){
+                    System.out.println("Please place row;colon;"+f+";\n Enter g for giving up \n" +
+                            "; make sure the row_index is in {1,2,3}; the col_index is in {1,2,3}; you can only place an" +f+"in your turn \n"+
+
+                            " Enter r to restart; \n"+
+                            " Enter e to exit");
+                    s = stdin.readLine();
+
+
+                }else if(s.equals("r")){
+//TODO r,e, don't exit when whin/draw;test input;user don't have to input x/o
+                }else if(s.equals("e")){
+                    System.out.println("Exit.");
+                    break;
+                }
 
                 while (s == null || s.length() == 0){
                     System.out.println("Input can't be null");
@@ -39,21 +53,21 @@ public class main {
                     if(param.length!=3 || param[2].length()!=1){
                         throw new Exception();
                     }
+                    if(m.isOccupied(Integer.parseInt(param[0]), Integer.parseInt(param[1]))){
+                        throw new Exception();
+                    }
                     m.upDateTable(Integer.parseInt(param[0]), Integer.parseInt(param[1]), param[2].charAt(0));
                     m.printTable();
                     player1= !player1;
 
                     if (m.getDraw()) {
                         System.out.println("TIE!");
-                        m.printTable();
                         break;
                     } else if (m.getP1win()) {
                         System.out.println("Congrats! P1 win!");
-                        m.printTable();
                         break;
                     } else if (m.getP2win()) {
                         System.out.println("congrats! P2 win!");
-                        m.printTable();
                         break;
                     }
 
@@ -61,7 +75,7 @@ public class main {
                 }catch(Exception e){
                     System.out.println("please make sure the row_index is in {1,2,3}; " +
                             "the col_index is in {1,2,3}; you can only place an '"+
-                            f+"' in your turn \n If you want to give up, please enter g; " +
+                            f+"' in your turn \n You are not allowed to overwrite an occupied one\nIf you want to give up, please enter g; " +
                             "if you want to restart, please enter r; if you want to exit, " +
                             "please enter e");
                 }
--- src/map.java
+++ src/map.java
@@ -1,28 +1,34 @@
+import javax.xml.bind.annotation.XmlList;
+
 import static java.lang.String.valueOf;
 
 public class map {
-    private static final char i=(char)32;
+    private static final char i = (char) 32;
+    private static final String TITLE = "*TIC TAC TOE*";
+    private static final String ROW = " --- --- ---";
+    private static final int TABLE_SIZE = 9;
+    private static final String LINE_0 = "  1   2   3  ";
+    public String table[];
     private char line1[];
     private char line2[];
     private char line3[];
-    public String table[];
-    private static final String TITLE="*TIC TAC TOE*";
-    private static final String ROW=" --- --- ---";
-    private static final int TABLE_SIZE=9;
-    private static final String LINE_0="  1   2   3  ";
     private boolean draw;
     private boolean p1win;
     private boolean p2win;
 
+    private int x;
+    private int o;
 
-    map(){
-        line1=new char[]{'|',i,i,i,'|',i,i,i,'|',i,i,i,'|','1'};
-        line2=new char[]{'|',i,i,i,'|',i,i,i,'|',i,i,i,'|','2'};
-        line3=new char[]{'|',i,i,i,'|',i,i,i,'|',i,i,i,'|','3'};
-        table=new String[]{TITLE, LINE_0, ROW, valueOf(line1), ROW, valueOf(line2),ROW,valueOf(line3),ROW};
-        draw=false;
-        p1win=false;
-        p2win=false;
+    map() {
+        line1 = new char[]{'|', i, i, i, '|', i, i, i, '|', i, i, i, '|', '1'};
+        line2 = new char[]{'|', i, i, i, '|', i, i, i, '|', i, i, i, '|', '2'};
+        line3 = new char[]{'|', i, i, i, '|', i, i, i, '|', i, i, i, '|', '3'};
+        table = new String[]{TITLE, LINE_0, ROW, valueOf(line1), ROW, valueOf(line2), ROW, valueOf(line3), ROW};
+        draw = false;
+        p1win = false;
+        p2win = false;
+        x=0;
+        o=0;
         printTable();
 
     }
@@ -31,58 +37,87 @@ public class map {
         return draw;
     }
 
+    public void setDraw(boolean draw) {
+        this.draw = draw;
+    }
+
     public boolean getP1win() {
         return p1win;
+    }
+
+    public void setP1win(boolean p1win) {
+        this.p1win = p1win;
     }
 
     public boolean getP2win() {
         return p2win;
     }
 
-    public void setDraw(boolean draw) {
-        this.draw = draw;
-    }
-
-
-    public void setP1win(boolean p1win) {
-        this.p1win = p1win;
-    }
-
     public void setP2win(boolean p2win) {
         this.p2win = p2win;
     }
 
-    public void printTable(){
-        for(int i=0; i<TABLE_SIZE; i++){
+    public void printTable() {
+        for (int i = 0; i < TABLE_SIZE; i++) {
             System.out.println(table[i]);
         }
     }
 
-    public void upDateTable(int r, int c, char f) throws Exception{
-//TODO Simpify the piece of logic ... string formater? and fix the inputs out of range;setDraw; setP1WIN; setP2WIN
-        if(  ( f=='x'||f=='o')) {
+    public boolean isOccupied(int r, int c){
+        boolean isOcp=table[r * 2 + 1].charAt(2 + 4 * (c - 1)) != i ? true: false;
+        System.out.println("ISOCCUP---"+isOcp);
+        return isOcp;
+    }
+
+    public void upDateTable(int r, int c, char f) throws Exception {
+//TODO Simpify the piece of logic ... string formater/string builder? setDraw; setP1WIN; setP2WIN; separate play1 and player 2
+        if (r>=1 && r<=3 && c>=1 && c<=3 && (f=='x' || f=='y')) {
             switch (r) {
                 case 1: {
-                    line1[2+4*(c-1)] = f;
-                    table[3]=valueOf(line1);
+                    line1[2 + 4 * (c - 1)] = f;
+                    table[r * 2 + 1] = valueOf(line1);
                     break;
                 }
                 case 2: {
-                    line2[2+4*(c-1)] = f;
-                    table[5]=valueOf(line2);
+                    line2[2 + 4 * (c - 1)] = f;
+                    table[r * 2 + 1] = valueOf(line2);
                     break;
                 }
                 case 3: {
-                    line3[2+4*(c-1)] = f;
-                    table[7]=valueOf(line3);
+                    line3[2 + 4 * (c - 1)] = f;
+                    table[r * 2 + 1] = valueOf(line3);
                     break;
                 }
                 default:
                     break;
             }
+            x='x'==f? x+1:x;
+            o='o'==f? o+1:o;
+            if(x+o!=9){
+                String line =table[r * 2 + 1];
+                int col =2 + 4 * (c - 1);
+                if((line.charAt(2)==line.charAt(6) && line.charAt(2)==line.charAt(10))
+                        || (line1[col] == line2[col] && line1[col] == line3[col])){
+                    p1win = f == 'x' ? true : false;
+                    p2win = f == 'o' ? true : false;
+                }
+                if ((r + c) == 4 || r==c) {
+                    if ((line1[2] == line2[6] && line1[2] == line3[10] && line1[2] != i)
+                            || (line1[10] == line2[6] && line1[10] == line3[2] &&line1[10] !=i )) {
+                        p1win = f == 'x' ? true : false;
+                        p2win = f == 'o' ? true : false;
+                    }
+                }
+            }
+            else{
+                draw=true;
+                p1win=false;
+                p2win=false;
+            }
 
 
-        }else{
+
+        } else {
             throw new Exception();
         }
     }
